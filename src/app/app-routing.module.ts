ed713c8b343d8c36e9158a195b81154e26d69556
Strevia <phillipnissen7@gmail.com>
2023-03-26 17:43:20 -0400

    Added basic toolbar and each page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { StoreComponent } from './store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent},
+    { path: 'store', component:StoreComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HomeComponent,
+    SearchComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
