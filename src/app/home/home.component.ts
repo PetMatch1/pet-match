6bd442f2fad3df04861a6a342ae982b0cea02116
MohamedAkash <mohamed.adel.akash@gmail.com>
2023-04-21 22:54:42 -0400

    Modal Creation- AI development in-Progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private router: Router){ }
+  selectedFile!: File;
+
+  constructor(private router: Router, private modalService: NgbModal) { }
+
   goToVotes($myParam: string = ''): void {
     const navigationDetails: string[] = ['http://localhost:4200/search'];
     if($myParam.length) {
       navigationDetails.push($myParam);
     }
     this.router.navigate(navigationDetails);
+  }
+
+  open(content: any): NgbModalRef {
+    return this.modalService.open(content);
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  upload(): void {
+    console.log('Uploading file...');
+    // Your upload logic here
   }
 }
