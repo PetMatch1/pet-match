3f57d5b4d298504b2f283e6336f773fad9a0b141
Nhanmo Flomo <njf9@njit.edu>
2023-04-23 15:53:44 -0400

    Create user now functional.

--- netlify/functions/create_user/create_user.js
+++ netlify/functions/create_user/create_user.js
@@ -1,36 +1,36 @@
 var User = require('../../../models/user');
-var bodyparser = require('body-parser');
+var express = require('express');
+
 const handler = async (event) => {
-    console.log(event.body);
     var mongoose = require('mongoose');
     mongoose.set("strictQuery", false);
     const dbURI = process.env.API_KEY;
-    async function main() {
+    try {
         await mongoose.connect(dbURI);
-    }
-    main().catch(err => console.log(err));
-    const formData = event.body;
-    console.log(formData.name);
+    } catch (err) {
+        console.log(err);
+    };
+    const formData = JSON.parse(event.body);
     const userdetail = {
         name: formData.name,
         email: formData.email,
         phone_num: formData.phone_num || null,
         bio: formData.bio || null
     };
-    User.create(userdetail).then(function(err, user) {
-        if (err) {
-            return {
-                statusCode: 500,
-                body: err.toString
-            };
-        } else {
-            return {
-                statusCode: 200,
-                body: user.JSON
-            }
+    var response;
+    await User.create(userdetail).then(function(user) {
+        response = {
+            statusCode: 200,
+            body: "New User Inserted!"
+        };
+    })
+    .catch(function(err) {
+        response = {
+            statusCode: 500,
+            body: err.toString
         };
     });
-  };
-  
-  module.exports = { handler }
-  
+    return response;
+}
+
+module.exports = { handler }
